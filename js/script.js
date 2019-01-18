8b6568cdc236e6be3cd1a904d17223544aae2a04
ChristianCoelho <coelho.christian@gmail.com>
2019-01-17 18:03:15 -0800

    script update

---
+++ js/script.js
@@ -0,0 +1,22 @@
+var name = "Christian Coelho";
+
+var age = 5;
+var array = [5,2,45,"abc", "grt"];
+
+var obj = {
+  firstName: "Christian",
+  lastName: "Coelho",
+  age: 10
+};
+
+//Type object
+//console.log("hi, I'm ", name);
+//console.log(" hi, I'm " + name);
+
+//var greeting = "hi, i'm " + name;
+//console.log(greeting);
+//var greet = "hi, I'm ";
+//var anotherGreeting = greet.concat(name);
+//console.log(anotherGreeting);
+
+console.log("hi, I'm ",obj.firstName)
